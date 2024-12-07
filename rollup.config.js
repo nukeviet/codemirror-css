924c1b4619a6ea9cd3688ff38d33fbf410fc4934
Phan Tấn Dũng <dungpt@vinades.vn>
2024-12-07 15:12:56 +0700

    Add banner for bundle file

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,17 @@ export default {
     file: "dist/codemirror.css.bundle.js", // Tệp đầu ra
     format: "iife", // Định dạng IIFE (trình duyệt)
     sourcemap: true, // Tạo file sourcemap để debug
-    name: "nukeviet.CSSEditor"
+    name: "nukeviet.CSSEditor",
+    banner: `
+/**
+ * NukeViet CodeMirror CSS
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2024 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version, see more CodeMirror license at https://codemirror.net/
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+    `
   },
   plugins: [
     resolve(), // Giúp tìm module
